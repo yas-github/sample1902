255d9ad94cef9ecec73b8f4332eea0005d89a38c
miyama <miyama@rails>
2019-02-24 23:27:50 +0000

    User途中

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users', to: 'users#index'
+  get 'user', to: 'users#show'
+  get 'users/:id', to: 'users#show'
+  get 'users/:id/edit', to: 'users#edit'
+
+  root to: 'tops#index'
+  get 'signup', to:'users#new'
+  resource :users
 end
